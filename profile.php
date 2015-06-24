157a29ac0b1fda5c7fd86aa0d0d4472005408556
darvishzadeh <darvishzadeh@gmail.com>
2015-06-24 23:28:36 +0800

    .

--- profile.php
+++ profile.php
@@ -1,113 +1,1 @@
-<?php
-if (!isset($_SESSION['user'])) {
-	header('location: ../?p=auth'); die();
-}
-?>
-<div class="row">
-	<div class="col-md-6 col-sm-6 col-md-offset-3 col-sm-offset-3">
-		<?php
-		if (isset($_GET['remove'])) {
-			$sth = $conn->prepare("DELETE FROM `users` WHERE `id` = ?");
-			$sth->execute(array($_SESSION['user']));
-			header('location: ./?p=logout');die();
-		}
-
-		if (!empty($_POST)) {
-			$subsc = NULL;
-			foreach ($_POST as $key => $value) {
-				if ($key != 'pwd')
-					$$key = htmlentities(trim($value));
-				if ($key == 'pwd')
-					$$key = $value;
-			}
-
-			$data = array(':name'		=> $full_name,
-										':email'	=> $email_addr,
-										':subsc'	=> $subsc,
-										':id'			=> $_SESSION['user']);
-
-			if (!empty($_POST['password'])) {
-				$pwd = '`password` = MD5(:pwd),';
-				$data[':pwd'] = $pwd;
-			} else {
-				$pwd = NULL;
-			}
-
-			if ($subsc == 'on')
-				$data[':subsc'] = 1;
-
-
-			try {
-				$sth = $conn->prepare("UPDATE `users`
-																	SET `name` = :name,
-																			`email` = :email,
-																			" . $pwd . "
-																			`subscription` = :subsc
-																			WHERE `id` = :id;");
-
-				$sth->execute($data);
-
-				echo '
-				<div class="alert alert-success" role="alert">
-					<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
-					<strong>Success!</strong> Your profile has been updated.
-				</div>';
-
-			} catch(PDOException $ex) {
-				echo '
-				<div class="alert alert-danger" role="alert">
-					<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-					<strong>Burp!</strong> There was something wrong with updating the your profile.' .
-					$ex->getMessage() . '
-				</div>';
-			}
-		}
-		?>
-	</div>
-</div>
-
-<?php
-
-$query = $conn->query("SELECT * FROM `users` WHERE `id` = '" . $_SESSION['user'] . "';");
-
-$row = $query->fetchAll(PDO::FETCH_ASSOC);
-
-if ($row) {
-?>
-
-<div class="row">
-	<div class="col-md-6 col-sm-6 col-md-offset-3 col-sm-offset-3 well">
-		<form action="./?<?php echo $_SERVER['QUERY_STRING'] ?>" method="post" autocomplete="off">
-			<h2>Edit Profile</h2>
-			<hr />
-					<div class="form-group">
-						<label for="full_name">Full Name</label>
-						<input type="text" class="form-control" name="full_name" placeholder="Full Name" minlength="2" maxlength="64" title="A name under 2 characters? We don't like it." value="<?php echo $row[0]['name'] ?>" required />
-					</div>
-					<div class="form-group">
-						<label for="email_addr">Email</label>
-						<input type="email" class="form-control" name="email_addr" placeholder="Email Address" maxlength="64" title="Ehem.. an email, you know?" value="<?php echo $row[0]['email'] ?>" required />
-					</div>
-					<div class="form-group">
-						<label for="password">Password</label>
-						<input type="password" class="form-control" name="pwd" placeholder="Password" minlength="6" title="Minimum 6 Characters." />
-					</div>
-					<div class="checkbox">
-						<label>
-							<input type="checkbox" name="subsc"<?php echo ($row[0]['subscription']==1?' checked':'') ?> /> Email Alert Subscription
-						</label>
-					</div><br />
-					<button type="submit" class="btn btn-primary pull-left">Update</button>
-					<a id="remover" data-warn="This action cannot be undone and your account will be removed permanently." href="./?p=profile&remove"><span class="label label-default pull-right">remove this account</span></a>
-				</div>
-			</div>
-		</form>
-	</div>
-</div>
-
-<?php
-
-} else {
-	require_once('404.php');
-}
-?>
+<?phpif (!isset($_SESSION['user'])) {	header('location: ./?p=auth'); die();}?><div class="row">	<div class="col-md-6 col-sm-6 col-md-offset-3 col-sm-offset-3">		<?php		if (isset($_GET['remove'])) {			$sth = $conn->prepare("DELETE FROM `users` WHERE `id` = ?");			$sth->execute(array($_SESSION['user']));			header('location: ./?p=logout');die();		}		if (!empty($_POST)) {			$subsc = NULL;			foreach ($_POST as $key => $value) {				if ($key != 'pwd')					$$key = htmlentities(trim($value));				if ($key == 'pwd')					$$key = $value;			}			$data = array(':name'		=> $full_name,										':email'	=> $email_addr,										':subsc'	=> $subsc,										':id'			=> $_SESSION['user']);			if (!empty($_POST['password'])) {				$pwd = '`password` = MD5(:pwd),';				$data[':pwd'] = $pwd;			} else {				$pwd = NULL;			}			if ($subsc == 'on')				$data[':subsc'] = 1;			try {				$sth = $conn->prepare("UPDATE `users`																	SET `name` = :name,																			`email` = :email,																			" . $pwd . "																			`subscription` = :subsc																			WHERE `id` = :id;");				$sth->execute($data);				echo '				<div class="alert alert-success" role="alert">					<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>					<strong>Success!</strong> Your profile has been updated.				</div>';			} catch(PDOException $ex) {				echo '				<div class="alert alert-danger" role="alert">					<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>					<strong>Burp!</strong> There was something wrong with updating the your profile.' .					$ex->getMessage() . '				</div>';			}		}		?>	</div></div><?php$query = $conn->query("SELECT * FROM `users` WHERE `id` = '" . $_SESSION['user'] . "';");$row = $query->fetchAll(PDO::FETCH_ASSOC);if ($row) {?><div class="row">	<div class="col-md-6 col-sm-6 col-md-offset-3 col-sm-offset-3 well">		<form action="./?<?php echo $_SERVER['QUERY_STRING'] ?>" method="post" autocomplete="off">			<h2>Edit Profile</h2>			<hr />					<div class="form-group">						<label for="full_name">Full Name</label>						<input type="text" class="form-control" name="full_name" placeholder="Full Name" minlength="2" maxlength="64" title="A name under 2 characters? We don't like it." value="<?php echo $row[0]['name'] ?>" required />					</div>					<div class="form-group">						<label for="email_addr">Email</label>						<input type="email" class="form-control" name="email_addr" placeholder="Email Address" maxlength="64" title="Ehem.. an email, you know?" value="<?php echo $row[0]['email'] ?>" required />					</div>					<div class="form-group">						<label for="password">Password</label>						<input type="password" class="form-control" name="pwd" placeholder="Password" minlength="6" title="Minimum 6 Characters." />					</div>					<div class="checkbox">						<label>							<input type="checkbox" name="subsc"<?php echo ($row[0]['subscription']==1?' checked':'') ?> /> Email Alert Subscription						</label>					</div><br />					<button type="submit" class="btn btn-primary pull-left">Update</button>					<a id="remover" data-warn="This action cannot be undone and your account will be removed permanently." href="./?p=profile&remove"><span class="label label-default pull-right">remove this account</span></a>				</div>			</div>		</form>	</div></div><?php} else {	require_once('404.php');}?>
--- station.php
+++ station.php
@@ -1,239 +1,1 @@
-<div class="row">
-	<h2>Data for station #<?php echo $_GET['id'] ?></h2>
-	<div class="col-md-10 col-sm-10">
-		<div id="dow_chart" style="width:100%; height:350px;"></div>
-	</div>
-	<div class="col-md-2 col-sm-2">
-		<input type=checkbox id=0 onClick="change(this)" checked>
-		<label for="0"> Temperature</label><br />
-
-		<input type=checkbox id=1 onClick="change(this)">
-		<label for="1"> Solar Radiation</label><br />
-
-		<input type=checkbox id=2 onClick="change(this)" checked>
-		<label for="2"> Humidity</label><br />
-
-		<input type=checkbox id=3 onClick="change(this)">
-		<label for="3"> Rain Meter</label><br />
-
-		<input type=checkbox id=4 onClick="change(this)">
-		<label for="4"> Wet Leaf</label><br />
-
-		<input type=checkbox id=5 onClick="change(this)">
-		<label for="5"> Soil Moisture</label><br />
-
-		<input type=checkbox id=6 onClick="change(this)">
-		<label for="6"> pH Meter</label><br />
-
-		<input type=checkbox id=7 onClick="change(this)">
-		<label for="7"> Wind Speed</label><br />
-
-		<hr />
-
-		<small>
-			- You can zoom by dragging on any part of the chart, horizontally or vertically.<br />
-			- Double click to reset the zoom.
-		</small>
-	</div>
-</div>
-
-<br />
-<br />
-<br />
-
-
-<div id="wind" style="min-width: 420px; max-width: 600px; height: 400px; margin: 0 auto"></div>
-
-<div style="display:none">
-	<table id="freq" border="0" cellspacing="0" cellpadding="0">
-		<tr nowrap bgcolor="#CCCCFF">
-			<th colspan="9" class="hdr">Table of Frequencies (percent)</th>
-		</tr>
-		<tr nowrap bgcolor="#CCCCFF">
-			<th class="freq">Direction</th>
-			<th class="freq">&lt; 0.5 m/s</th>
-			<th class="freq">0.5-2 m/s</th>
-			<th class="freq">2-4 m/s</th>
-			<th class="freq">4-6 m/s</th>
-			<th class="freq">6-8 m/s</th>
-			<th class="freq">8-10 m/s</th>
-			<th class="freq">&gt; 10 m/s</th>
-			<th class="freq">Total</th>
-		</tr>
-		<tr nowrap>
-			<td class="dir">N</td>
-			<td class="data">1.81</td>
-			<td class="data">1.78</td>
-			<td class="data">0.16</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">3.75</td>
-		</tr>
-		<tr nowrap>
-			<td class="dir">NE</td>
-			<td class="data">0.82</td>
-			<td class="data">0.82</td>
-			<td class="data">0.07</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">1.71</td>
-		</tr>
-		<tr nowrap>
-			<td class="dir">E</td>
-			<td class="data">0.62</td>
-			<td class="data">2.20</td>
-			<td class="data">0.49</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">3.32</td>
-		</tr>
-		<tr nowrap>
-			<td class="dir">SE</td>
-			<td class="data">1.61</td>
-			<td class="data">3.06</td>
-			<td class="data">2.37</td>
-			<td class="data">2.14</td>
-			<td class="data">1.74</td>
-			<td class="data">0.39</td>
-			<td class="data">0.13</td>
-			<td class="data">11.45</td>
-		</tr>
-		<tr nowrap>
-			<td class="dir">S</td>
-			<td class="data">2.66</td>
-			<td class="data">4.74</td>
-			<td class="data">0.43</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">7.83</td>
-		</tr>
-		<tr nowrap>
-			<td class="dir">SW</td>
-			<td class="data">2.53</td>
-			<td class="data">4.01</td>
-			<td class="data">1.22</td>
-			<td class="data">0.49</td>
-			<td class="data">0.13</td>
-			<td class="data">0.00</td>
-			<td class="data">0.00</td>
-			<td class="data">8.39</td>
-		</tr>
-		<tr nowrap>
-			<td class="dir">W</td>
-			<td class="data">1.64</td>
-			<td class="data">1.71</td>
-			<td class="data">0.92</td>
-			<td class="data">1.45</td>
-			<td class="data">0.26</td>
-			<td class="data">0.10</td>
-			<td class="data">0.00</td>
-			<td class="data">6.09</td>
-		</tr>
-		<tr nowrap>
-			<td class="dir">NW</td>
-			<td class="data">1.58</td>
-			<td class="data">4.28</td>
-			<td class="data">1.28</td>
-			<td class="data">0.76</td>
-			<td class="data">0.66</td>
-			<td class="data">0.69</td>
-			<td class="data">0.03</td>
-			<td class="data">9.28</td>
-		</tr>		
-		<tr nowrap>
-			<td class="totals">Total</td>
-			<td class="totals">25.53</td>
-			<td class="totals">44.54</td>
-			<td class="totals">15.07</td>
-			<td class="totals">8.52</td>
-			<td class="totals">4.31</td>
-			<td class="totals">1.81</td>
-			<td class="totals">0.23</td>
-			<td class="totals">&nbsp;</td>
-		</tr>
-	</table>
-</div>
-
-<?php
-if (isset($_SESSION['user'])) {
-?>
-<br />
-<hr />
-
-<div class="row">
-	<?php
-	if (isset($_POST['comment']) && !empty($_POST['comment'])) {
-		$data = array(':user_id'	=> $_SESSION['user'],
-					  ':station_id'	=> $_GET['id'],
-					  ':comment'	=> $_POST['comment']);
-		$query = $conn->prepare("INSERT INTO `comments`
-								(`user_id`,`station_id`,`comment`) VALUES
-								(:user_id, :station_id, :comment);");
-
-		if (!$query->execute($data)) {
-			echo '
-				<div class="col-md-10 col-sm-10 col-md-offset-1 col-sm-offset-1">
-					<div class="alert alert-danger" role="alert">
-						<span class="glyphicon glyphicon-ban-circle" aria-hidden="true"></span>
-						<strong>Achoo!</strong> Something\'s wrong with the commenting system :/
-					</div>
-				</div>';
-		}
-	}
-	?>
-	<div class="col-md-10 col-sm-10 col-md-offset-1 col-sm-offset-1">
-		<h2>User Comments</h2>
-
-		<?php
-		// echo user comments yo
-		
-		if (isset($_SESSION['user'])) {
-		?>
-		<div class="row">
-			<form method="post">
-				<div class="form-group">
-					<textarea class="form-control" name="comment" placeholder="Comment.." minlength="10" required rows="5"></textarea>
-				</div>
-				<button type="submit" class="btn btn-primary pull-right">Send</button>
-			</form>
-		</div>
-		<?php
-		}
-		?>
-</div>
-
-<?php
-}
-?>
-
-
-<script type="text/javascript" src="./js/dygraph-combined.js"></script>
-
-
-<script type="text/javascript">
-agrometChart = new Dygraph(
-	document.getElementById('dow_chart'),
-	"station_data.php?id=<?php echo $_GET['id'] ?>",
-	{
-		showRoller: true,
-		labelsKMB: true,
-		rollPeriod: 7,
-		visibility: [true, false, true, false, false, false, false, false],
-		showRangeSelector: true
-	}
-);
-
-function change(el) {
-	agrometChart.setVisibility(el.id, el.checked);
-}
-
-</script>
-
+<div class="row">	<h2>Data for station #<?php echo $_GET['id'] ?></h2>	<div class="col-md-10 col-sm-10">		<div id="dow_chart" style="width:100%; height:350px;"></div>	</div>	<div class="col-md-2 col-sm-2">		<input type=checkbox id=0 onClick="change(this)" checked>		<label for="0"> Temperature</label><br />		<input type=checkbox id=1 onClick="change(this)">		<label for="1"> Solar Radiation</label><br />		<input type=checkbox id=2 onClick="change(this)" checked>		<label for="2"> Humidity</label><br />		<input type=checkbox id=3 onClick="change(this)">		<label for="3"> Rain Meter</label><br />		<input type=checkbox id=4 onClick="change(this)">		<label for="4"> Wet Leaf</label><br />		<input type=checkbox id=5 onClick="change(this)">		<label for="5"> Soil Moisture</label><br />		<input type=checkbox id=6 onClick="change(this)">		<label for="6"> pH Meter</label><br />		<input type=checkbox id=7 onClick="change(this)">		<label for="7"> Wind Speed</label><br />		<hr />		<small>			- You can zoom by dragging on any part of the chart, horizontally or vertically.<br />			- Double click to reset the zoom.		</small>	</div></div><br /><br /><br /><div id="wind" style="min-width: 420px; max-width: 600px; height: 400px; margin: 0 auto"></div><div style="display:none">	<table id="freq" border="0" cellspacing="0" cellpadding="0">		<tr nowrap bgcolor="#CCCCFF">			<th colspan="9" class="hdr">Table of Frequencies (percent)</th>		</tr>		<tr nowrap bgcolor="#CCCCFF">			<th class="freq">Direction</th>			<th class="freq">&lt; 0.5 m/s</th>			<th class="freq">0.5-2 m/s</th>			<th class="freq">2-4 m/s</th>			<th class="freq">4-6 m/s</th>			<th class="freq">6-8 m/s</th>			<th class="freq">8-10 m/s</th>			<th class="freq">&gt; 10 m/s</th>			<th class="freq">Total</th>		</tr>		<tr nowrap>			<td class="dir">N</td>			<td class="data">1.81</td>			<td class="data">1.78</td>			<td class="data">0.16</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">3.75</td>		</tr>		<tr nowrap>			<td class="dir">NE</td>			<td class="data">0.82</td>			<td class="data">0.82</td>			<td class="data">0.07</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">1.71</td>		</tr>		<tr nowrap>			<td class="dir">E</td>			<td class="data">0.62</td>			<td class="data">2.20</td>			<td class="data">0.49</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">3.32</td>		</tr>		<tr nowrap>			<td class="dir">SE</td>			<td class="data">1.61</td>			<td class="data">3.06</td>			<td class="data">2.37</td>			<td class="data">2.14</td>			<td class="data">1.74</td>			<td class="data">0.39</td>			<td class="data">0.13</td>			<td class="data">11.45</td>		</tr>		<tr nowrap>			<td class="dir">S</td>			<td class="data">2.66</td>			<td class="data">4.74</td>			<td class="data">0.43</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">7.83</td>		</tr>		<tr nowrap>			<td class="dir">SW</td>			<td class="data">2.53</td>			<td class="data">4.01</td>			<td class="data">1.22</td>			<td class="data">0.49</td>			<td class="data">0.13</td>			<td class="data">0.00</td>			<td class="data">0.00</td>			<td class="data">8.39</td>		</tr>		<tr nowrap>			<td class="dir">W</td>			<td class="data">1.64</td>			<td class="data">1.71</td>			<td class="data">0.92</td>			<td class="data">1.45</td>			<td class="data">0.26</td>			<td class="data">0.10</td>			<td class="data">0.00</td>			<td class="data">6.09</td>		</tr>		<tr nowrap>			<td class="dir">NW</td>			<td class="data">1.58</td>			<td class="data">4.28</td>			<td class="data">1.28</td>			<td class="data">0.76</td>			<td class="data">0.66</td>			<td class="data">0.69</td>			<td class="data">0.03</td>			<td class="data">9.28</td>		</tr>				<tr nowrap>			<td class="totals">Total</td>			<td class="totals">25.53</td>			<td class="totals">44.54</td>			<td class="totals">15.07</td>			<td class="totals">8.52</td>			<td class="totals">4.31</td>			<td class="totals">1.81</td>			<td class="totals">0.23</td>			<td class="totals">&nbsp;</td>		</tr>	</table></div><?phpif (isset($_SESSION['user'])) {?><br /><hr /><div class="row">	<?php	if (isset($_POST['comment']) && !empty($_POST['comment'])) {		$data = array(':user_id'	=> $_SESSION['user'],					  ':station_id'	=> $_GET['id'],					  ':comment'	=> $_POST['comment']);		$query = $conn->prepare("INSERT INTO `comments`								(`user_id`,`station_id`,`comment`) VALUES								(:user_id, :station_id, :comment);");		if (!$query->execute($data)) {			echo '				<div class="col-md-10 col-sm-10 col-md-offset-1 col-sm-offset-1">					<div class="alert alert-danger" role="alert">						<span class="glyphicon glyphicon-ban-circle" aria-hidden="true"></span>						<strong>Achoo!</strong> Something\'s wrong with the commenting system :/					</div>				</div>';		}	}	?>	<div class="col-md-10 col-sm-10 col-md-offset-1 col-sm-offset-1">		<h2>User Comments</h2>		<?php		$query = $conn->prepare("SELECT `users`.`name`, `users`.`email`, `comments`.`created_on`, `comments`.`comment`														 FROM `comments`														 LEFT JOIN `users`														 ON `comments`.`user_id` = `users`.`id`														 WHERE `comments`.`station_id` = ?");		$query->execute(array($_GET['id']));		$row = $query->fetchAll(PDO::FETCH_ASSOC);		foreach ($row as $value) {			echo '<div class="row">							<div class="comment_owner">								<span class="avatar">' . grav($value['email'], 48, 'mm', 'g', true) . '</span>								<span class="name">' . $value['name'] . '									<small class="datetime text-info">' . $value['created_on'] . '</small>								</span>							</div>							<div class="comment_content">' . nl2br($value['comment']) . '</div>						</div>';		}				if (isset($_SESSION['user'])) {		?>		<br />		<div class="row">			<form method="post">				<div class="form-group">					<textarea class="form-control" name="comment" placeholder="Comment.." minlength="10" required rows="5"></textarea>				</div>				<button type="submit" class="btn btn-primary pull-right">Send</button>			</form>		</div>		<?php		}		?></div><?php}?><script type="text/javascript" src="./js/dygraph-combined.js"></script><script type="text/javascript">agrometChart = new Dygraph(	document.getElementById('dow_chart'),	"station_data.php?id=<?php echo $_GET['id'] ?>",	{		showRoller: true,		labelsKMB: true,		rollPeriod: 7,		visibility: [true, false, true, false, false, false, false, false],		showRangeSelector: true	});function change(el) {	agrometChart.setVisibility(el.id, el.checked);}</script>
